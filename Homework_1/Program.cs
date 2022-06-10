85e61fcc5803c275d4a5cfc7885945318509d377
Andrey <sergeev.sergeev-and@yandex.ru>
2022-06-10 11:59:26 +0700

    Add seminar 06.06.2022

---
+++ Homework_1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int NumberInput(string text)//Метод ввода и проверки на число
+{
+    bool isInputInt = true;
+    int number =0;
+    while (isInputInt)
+    {
+        Console.Write($"Введите {text} :");
+        string numberSTR = Console.ReadLine();
+        if (int.TryParse(numberSTR, out int numberInt))
+        {
+            if (numberInt <= 0) Console.WriteLine("Введите число больше нуля");
+            else
+            {
+                number = numberInt;
+                isInputInt = false;
+            } 
+        }
+        else 
+            Console.WriteLine("Ввели не число");
+    }
+    return number;
+}
+
+int NumbersSumm(int numberN,int numberM)
+{
+    if (numberN >= numberM)
+    return numberM;
+    numberM--;
+    return numberM+1 + NumbersSumm(numberN, numberM);
+}
+
+int numberN = NumberInput("числоN");
+int numberM = NumberInput("числоM");
+
+int result = NumbersSumm(numberN, numberM); 
+Console.WriteLine(result);
---
+++ Homework_2/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+*/
+
+
+int NumberInput(string text)//Метод ввода и проверки на число
+{
+    bool isInputInt = true;
+    int number =0;
+    while (isInputInt)
+    {
+        Console.Write($"Введите {text} :");
+        string numberSTR = Console.ReadLine();
+        if (int.TryParse(numberSTR, out int numberInt))
+        {
+            if (numberInt < 0) Console.WriteLine("Введите положительное число");
+            else
+            {
+                number = numberInt;
+                isInputInt = false;
+            }
+            
+        }
+        else 
+            Console.WriteLine("Ввели не число");
+    }
+    return number;
+}
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+    return n + 1;
+    if (m != 0 && n == 0)
+    return Ackermann(m - 1, 1);
+    if (m > 0 && n > 0)
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+    return Ackermann(m, n);
+}   
+int m = NumberInput("число m");
+int n = NumberInput("число n");
+long acermann = Ackermann(m, n);
+Console.WriteLine($"m = {m}, n = {n} -> A({m},{n}) = {acermann}");
